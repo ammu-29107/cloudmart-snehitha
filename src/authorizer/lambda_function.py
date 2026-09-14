9439ace5f3471bb2704130c954cf783680d12c66
Snehitha Reddy G (Omnicom) <snehitha.reddyg@omc.com>
2026-09-14 13:03:39 +0530

    Fix public customer registration authorization

--- src/authorizer/lambda_function.py
+++ src/authorizer/lambda_function.py
@@ -416,7 +416,7 @@ def lambda_handler(event, context):
                     }
                 )
 
-        supplied_token = supplied_token.strip()
+        supplied_token = supplied_token.strip() if supplied_token else None
 
         if not supplied_token:
 
